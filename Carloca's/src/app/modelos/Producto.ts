3c9492d8d53170e4965d50b1985c880d8cb932eb
Gabriela-Serrano-Lopez <75129974+Gabriela-Serrano-Lopez@users.noreply.github.com>
2021-06-15 18:21:36 -0500

    main

--- Carloca's/src/app/modelos/Producto.ts
+++
@@ -1,7 +0,0 @@
-export class Producto {
-    cantidad:number;
-    des: string;
-    fecha: string;
-    nombre: string;
-    precio: number;
-}
--- Carloca's/src/app/services/check-session.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class CheckSessionService {
-  inSession: boolean = false;
-  constructor() { }
-}
